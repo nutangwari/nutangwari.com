141b2a7feb7f0bf64d97f79fdc55d96addf8392f
Nutan Gwari <nutan.kgwari@gmail.com>
2023-07-11 02:09:28 +0530

    Enabled analytics in application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Nav from '@/components/Nav'
 import './globals.css'
 import { Inter, Josefin_Sans } from 'next/font/google'
 import Footer from '@/components/Footer'
+import { Analytics } from '@vercel/analytics/react';
 
 // const inter = Inter({ subsets: ['latin'] })
 const josefin = Josefin_Sans({
@@ -27,7 +28,10 @@ export default function RootLayout({
         </nav>
 
         {/* removing the margin mt-20 (5rem) from screen height */}
-        <main className='mt-20 w-full flex-grow px-10 mx-auto'>{children}</main>
+        <main className='mt-20 w-full flex-grow px-10 mx-auto'>
+          {children}
+          <Analytics />
+        </main>
 
         <Footer />
       </body>
